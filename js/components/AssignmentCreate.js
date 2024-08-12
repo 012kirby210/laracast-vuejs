aa7e20721781425f5a8d8119ee80880ca5ec1c14
012kirby210 <012kirby210@gmail.com>
2024-08-12 21:05:07 +0200

    Correction bug graphique sur l'input text.

--- js/components/AssignmentCreate.js
+++ js/components/AssignmentCreate.js
@@ -1,11 +1,8 @@
 export default {
     template: `
-        <form @submit.prevent="add">
-            <div class="border border-gray-600 text-black flex">
-                <input 
-                v-model="newAssignment" 
-                placeholder="New assignmetn..." 
-                class="p-2">
+        <form @submit.prevent="add" class="flex">
+            <div class="border border-gray-600 text-black flex w-full">
+                <input v-model="newAssignment" placeholder="New assignment..." class="p-2 min-w-0"/>
                 <button type="submit" class="bg-white p-2 border-l">Add</button>
             </div>
         </form>
