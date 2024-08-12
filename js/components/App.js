a7d5a0ee5f086cf426dc264fbf338e13a59ec1d4
012kirby210 <012kirby210@gmail.com>
2024-08-12 21:05:25 +0200

    Encapsulation de la présentation dans des panneaux.

--- js/components/App.js
+++ js/components/App.js
@@ -1,9 +1,13 @@
 import Assignments from './Assignments.js'
+import Panel from './Panel.js'
 
 export default {
-    components: { Assignments },
+    components: { Assignments, Panel },
     template: `
-         <Assignments></Assignments>
+        <div class="grid gap-6">
+            <Assignments></Assignments>
+        </div>
+         
     `,
 
 }
--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,29 +1,33 @@
 import Assignment from "./Assignment.js";
 import AssignmentTags from "./AssignmentTags.js";
+import Panel from './Panel.js';
 export default {
-    components: { Assignment, AssignmentTags },
+    components: { Assignment, AssignmentTags, Panel },
     template: `
-    <section v-show="assignments.length" class="w-60">
-        <div class="flex justify-between items-start">
-            <h2 class="font-bold mb-2">
-                {{ sectionTitle }}
-                <span>({{assignments.length}})</span>
-            </h2>
-            <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
-        </div>
-        <AssignmentTags 
-        v-model:currentTag="currentTag"
-        :initial-tags="this.assignments.map( a => a.tag )"
-        ></AssignmentTags>
-        <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-            <Assignment 
-                v-for="assignment in filteredAssignements"
-                :key="assignment.id"
-                :assignment="assignment" 
-            ></Assignment>
-        </ul>
-        <slot></slot>
-    </section>
+        <Panel v-show="assignments.length" class="w-60">
+            <div class="flex justify-between items-start">
+                <h2 class="font-bold mb-2">
+                    {{ sectionTitle }}
+                    <span>({{assignments.length}})</span>
+                </h2>
+                <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+            </div>
+            <AssignmentTags 
+            v-model:currentTag="currentTag"
+            :initial-tags="this.assignments.map( a => a.tag )"
+            ></AssignmentTags>
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+                <Assignment 
+                    v-for="assignment in filteredAssignements"
+                    :key="assignment.id"
+                    :assignment="assignment" 
+                ></Assignment>
+            </ul>
+            <slot></slot>
+            <template #footer>
+                Copyright J.W.
+            </template>
+        </Panel>
     `,
     props: {
         sectionTitle: String,
